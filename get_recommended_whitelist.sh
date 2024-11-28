f516ba0acd220fd8659468e737adbc9a2fd88ca4
npdkhang2 <npdkhang2@gmail.com>
2024-11-28 14:31:53 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
+https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
+https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
+https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
+
 node download_lists.js allowlist
